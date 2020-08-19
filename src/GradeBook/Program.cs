8864b84bd0f92b8e49d626d327bad7416c1bf5c9
Erin Claudio <eclaudio@gmail.com>
2020-08-19 16:07:59 -0400

    working with basic data structures

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -4,12 +4,25 @@ namespace GradeBook
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            if(args.Length > 0)
+            static void Main(string[] args)
             {
-            Console.WriteLine($"Hello {args[0]}!");
+                var numbers = new []{12.6, 10.3, 13.1};
+                var sum = 0.0;
+                foreach(double number in numbers)
+                {
+                   sum += number; 
+                }
+
+                Console.WriteLine(sum); 
+
+                if(args.Length > 0)
+                {
+                    Console.WriteLine($"Hello {args[0]}!");
+                }
+                else
+                {
+                    Console.WriteLine($"Hello");  
+                }
             }
-        }
     }
 }
